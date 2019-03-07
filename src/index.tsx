7e607586bca999fc76f4cb724e4f316b2aaed72b
Nico Peham <nico96.peham@gmail.com>
2019-03-07 12:51:22 +0100

    Vanilla tslint (#1)
    * working setup
    
    * move index html to top level folder
    
    * fix quotemark
    
    * move express to devDependencies

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+declare let module: any;
+
+import { App } from './App';
+
+const RenderApp = () => <App />;
+
+ReactDOM.render(<RenderApp />, document.getElementById('root') as HTMLElement);
+
+if (module.hot) {
+  module.hot.accept();
+}
