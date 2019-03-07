9d986db5a05e8679f8a1b38c5e755be41b50b2ac
npeham <nico96.peham@gmail.com>
2019-03-07 12:43:27 +0100

    setup enzyme and add example test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  // Setup Enzyme
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"]
 };
