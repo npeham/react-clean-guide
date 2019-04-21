cf51c7c0606421accab4e95ec121cbf2b97f5462
npeham <nico96.peham@gmail.com>
2019-04-21 16:53:35 +0200

    refactoring

--- src/shared/helper/routing.helper.test.ts
+++ src/shared/helper/routing.helper.test.ts
@@ -11,41 +11,32 @@ import {
 } from '../../modules/user/user.routing';
 
 describe('routing helper tests', () => {
+  const userEditScene: UserEditScene = {
+    path: UserRoute.Edit,
+    params: { projId: '3', userId: '8' },
+  };
+
+  const userListScene: UserListScene = {
+    path: UserRoute.List,
+  };
+
   describe('getRoutePath', () => {
     it('should return the correct route path computed from path and defaultParams of scene', () => {
-      const userEditScene: UserEditScene = {
-        path: UserRoute.Edit,
-        params: { projId: '', userId: '' },
-      };
-
       expect(getRoutePath(userEditScene)).toBe('/user/edit/:projId/:userId');
     });
 
     // tslint:disable-next-line:max-line-length
     it('should return the correct route path computed from path and defaultParams of scene with no params', () => {
-      const userListScene: UserListScene = {
-        path: UserRoute.List,
-      };
-
       expect(getRoutePath(userListScene)).toBe('/user/list');
     });
   });
 
   describe('getPathWithParams', () => {
     it('returns the correct path with params set', () => {
-      const userEditScene: UserEditScene = {
-        path: UserRoute.Edit,
-        params: { projId: '3', userId: '8' },
-      };
-
       expect(getPathWithParams(userEditScene)).toBe('/user/edit/3/8');
     });
 
     it('returns the correct path with no params set', () => {
-      const userListScene: UserListScene = {
-        path: UserRoute.List,
-      };
-
       expect(getPathWithParams(userListScene)).toBe('/user/list');
     });
   });
@@ -54,11 +45,6 @@ describe('routing helper tests', () => {
     it('throws an error because path is already existing', () => {
       const existingPaths = ['/user/edit/:projId/:userId', '/user/list'];
 
-      const userEditScene: UserEditScene = {
-        path: UserRoute.Edit,
-        params: { projId: '', userId: '' },
-      };
-
       const t = () => verifyIfPathOfSceneIsUnique(userEditScene, existingPaths);
 
       expect(t).toThrowError(RoutingError.DuplicatePath);
@@ -66,11 +52,6 @@ describe('routing helper tests', () => {
 
     it('returns the correct route path', () => {
       const existingPaths = ['/user/list'];
-
-      const userEditScene: UserEditScene = {
-        path: UserRoute.Edit,
-        params: { projId: '', userId: '' },
-      };
 
       expect(verifyIfPathOfSceneIsUnique(userEditScene, existingPaths)).toBe(
         '/user/edit/:projId/:userId',
