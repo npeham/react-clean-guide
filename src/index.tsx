4dabdb71053571644b8287e370d9d8b0fddf25eb
npeham <nico96.peham@gmail.com>
2019-04-25 22:49:45 +0200

    implement configureStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 declare let module: any;
 
 import { App } from './App';
+import { store } from './configureStore';
 
 const RenderApp = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(<RenderApp />, document.getElementById('root') as HTMLElement);
